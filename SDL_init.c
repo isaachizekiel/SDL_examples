646515309a507e3863e5d25bb80841fde9c18b6f
izak <isaachizekiel@gmail.com>
2022-10-27 12:23:42 +0300

    init

---
+++ SDL_init.c
@@ -0,0 +1,23 @@
+#include <SDL_error.h>
+#include <stdio.h>
+#include <SDL.h>
+
+int main() {
+    printf("Initializing SDL \n");
+
+    /* Initializing the defaults */
+    if (SDL_Init(SDL_INIT_VIDEO || SDL_INIT_AUDIO == -1)) {
+      printf("could not initialize SDL %s \n", SDL_GetError());
+      return -1;
+    }
+
+    printf("SDL initialized \n");
+
+    /* shutdown subsystems */
+    SDL_Quit();
+
+    printf("quiting ...\n");
+    
+    
+    return 0;
+}
